db4bbf5714f98d2d497e9f62d835e275ecca780c
María <mariaefm@gmail.com>
2012-02-24 17:46:32 +0100

    Modificado el modelo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoMaria::Application.config.secret_token = 'a2bb759d7f6159bed8c8dfec1282c1c787f2b26734752f899bd3ef8150e6a33e878e992e33e06b864a19e0b4792311ecc72898bc0652a8c677536a88004919c3'
